898780e1e64dbd00491ce6eb83f3247726b8cb52
Devashen Govender <devashen007@gmail.com>
2023-12-04 00:42:17 +0200

    Edited WebAR

--- Assets/_PROJECT/Scripts/InitializeAR.cs
+++ Assets/_PROJECT/Scripts/InitializeAR.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InitializeAR : MonoBehaviour
 {
+    #region VARIABLES 
+    [SerializeField]
+    public Button button;
+    #endregion
+
+    #region UNITY EVENTS
     // Start is called before the first frame update
     void Start()
     {
-        
+        button.onClick.Invoke();
     }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
