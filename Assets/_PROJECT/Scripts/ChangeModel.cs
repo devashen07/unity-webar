5d39448e24848da33a4d780e12ae2000de985147
Devashen Govender <devashen007@gmail.com>
2023-12-04 01:39:49 +0200

    Final WebAR

--- Assets/_PROJECT/Scripts/ChangeModel.cs
+++ Assets/_PROJECT/Scripts/ChangeModel.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ChangeModel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region VARIABLES 
+    [SerializeField]
+    private List<GameObject> listOfModels;
+
+    private int index = 0;
+    #endregion
+
+    #region Public API
+    public void OnClickNext()
     {
+        listOfModels[index].SetActive(false); 
+        if (index < listOfModels.Count - 1)
+        {
+            index += 1; 
+        }
+        else
+        {
+            index = 0;  
+        }
+
+        listOfModels[index].SetActive(true);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickPrevious()
     {
-        
+        listOfModels[index].SetActive(false);
+        if (index != 0)
+        {
+            index -= 1;
+        }
+        else
+        {
+            index = listOfModels.Count - 1;
+        }
+
+        listOfModels[index].SetActive(true);
     }
+    #endregion
+
+    #region UNITY EVENTS
+
+    public void Start()
+    {
+        index = 0; 
+    }
+    #endregion
 }
